8daa86840c00b465a82cabb8f6c172c1de4fd9f2
GOWSICK200522 <gowsick222@gamil.com>
2025-08-06 10:53:47 +0530

    file test

---
+++ pull.java
@@ -0,0 +1,14 @@
+
+public class pull {
+   public static void main(String[] args) {
+      byte var1 = 5;
+      byte var2 = 15;
+      System.out.println(var1 == var2);
+      System.out.println(var1 != var2);
+      System.out.println(var1 > var2);
+      System.out.println(var1 < var2);
+      System.out.println(var1 >= var2);
+      System.out.println(var1 <= var2);
+   }
+}
+
